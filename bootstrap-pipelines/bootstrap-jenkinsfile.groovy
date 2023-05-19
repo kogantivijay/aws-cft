3e43733f31edc24a9bdf32b577c51ee6a5ad52f4
Vijay Koganti <15212181+kogantivijay@users.noreply.github.com>
2023-05-19 10:46:14 +0100

    Prompt for Input

--- bootstrap-pipelines/bootstrap-jenkinsfile.groovy
+++ bootstrap-pipelines/bootstrap-jenkinsfile.groovy
@@ -14,6 +14,53 @@ properties([
 pipeline {
     agent any
     stages {
+        stage('CR number validation') {
+            when {
+                expression { params.TARGET_ENV.toLowerCase().contains('pdn') }
+            }
+            steps {
+                script {
+                    def validCRNumberRegex = ~/^CHG\d{10}$/
+                    // If CR number is invalid or default, ask for user input
+                    if (!params.CR_NUMBER_PROD.matches(validCRNumberRegex)) {
+                        params.CR_NUMBER_PROD = promptForCRNumber(validCRNumberRegex)
+                    }
+                }
+            }
+        }
+
+def promptForCRNumber(validCRNumberRegex) {
+    def userInput = input message: 'CR number for Prod deployment is invalid or default. Please provide a valid CR number:',
+        id: 'crNumberInput',
+        ok: 'Proceed',
+        parameters: [
+            string(
+                defaultValue: '', 
+                description: 'CR Number for Prod Deployment', 
+                name: 'CR_Number', 
+                trim: true
+            )
+        ]
+
+    while (!userInput.CR_Number.matches(validCRNumberRegex)) {
+        userInput = input message: 'CR number for Prod deployment is invalid. Please provide a valid CR number:',
+            id: 'crNumberInput',
+            ok: 'Proceed',
+            parameters: [
+                string(
+                    defaultValue: '', 
+                    description: 'CR Number for Prod Deployment', 
+                    name: 'CR_Number', 
+                    trim: true
+                )
+            ]
+    }
+    return userInput.CR_Number
+}
+
+        
+
+
         stage('SCM Checkout') {
             steps {
                 checkout scm
