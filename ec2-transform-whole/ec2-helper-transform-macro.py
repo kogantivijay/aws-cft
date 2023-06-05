3c3f214bb720f95dde5697831553c669e79a3951
Vijay Koganti <15212181+kogantivijay@users.noreply.github.com>
2023-06-05 08:27:10 +0100

    Add support for SSM Param

--- ec2-transform-whole/ec2-helper-transform-macro.py
+++ ec2-transform-whole/ec2-helper-transform-macro.py
@@ -1,8 +1,13 @@
+import boto3
 import json
 import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+ssm = boto3.client('ssm')
+parameter = ssm.get_parameter(Name='/app/dsdevops/common/kms/cmk-ebs-key', WithDecryption=True)
+KMS_KEY_ARN = parameter['Parameter']['Value']
 
 def handler(event, context):
     try:
@@ -10,36 +15,39 @@ def handler(event, context):
 
         fragment = event['fragment']
         volumes_data = json.loads(event['templateParameterValues']['VolumesJson'])
-        ec2_instance_id = {"Ref": "Instance"}
-        ec2_instance_az = {"Fn::GetAtt": ["Instance", "AvailabilityZone"]}
+        ec2_instance_id = {"Ref": "EC2Instance"}
+        ec2_instance_az = {"Fn::GetAtt": ["EC2Instance", "AvailabilityZone"]}
 
         resources = fragment['Resources']
 
-        block_device_mappings = []
+        block_device_mappings = resources["EC2Instance"]["Properties"].get("BlockDeviceMappings", [])
+
         for idx, volume in enumerate(volumes_data):
             is_root_volume = volume.get("RootVolume", False)
+            ebs_data = {
+                "VolumeSize": volume.get("Size", None),
+                "VolumeType": volume.get("VolumeType", None),
+                "Iops": volume.get("Iops", None),
+                "KmsKeyId": KMS_KEY_ARN,
+                "Encrypted": True
+            }
 
             if is_root_volume:
-                block_device_mappings.append({
-                    "DeviceName": volume["Device"],
-                    "Ebs": {
-                        "VolumeSize": volume["Size"],
-                        "VolumeType": volume["VolumeType"],
-                        "Iops": volume["Iops"]
-                    }
-                })
+                if block_device_mappings:
+                    block_device_mappings[0]["Ebs"].update(
+                        {k: v for k, v in ebs_data.items() if v is not None}
+                    )
             else:
+                ebs_data["Throughput"] = volume.get("Throughput", None) if volume.get("VolumeType", "") == "gp3" else None
+
                 volume_resource_name = f"Volume{idx}"
                 attachment_resource_name = f"VolumeAttachment{idx}"
 
                 resources[volume_resource_name] = {
                     "Type": "AWS::EC2::Volume",
-                    "Properties": {
-                        "Size": volume["Size"],
-                        "AvailabilityZone": ec2_instance_az,
-                        "VolumeType": volume["VolumeType"],
-                        "Iops": volume["Iops"],
-                    }
+                    "DeletionPolicy": "Retain",
+                    "UpdateReplacePolicy": "Retain",
+                    "Properties": ebs_data
                 }
 
                 resources[attachment_resource_name] = {
@@ -52,9 +60,6 @@ def handler(event, context):
                 }
 
                 logger.info(f"Added volume {volume_resource_name} and attachment {attachment_resource_name}")
-
-        if block_device_mappings:
-            resources["Instance"]["Properties"]["BlockDeviceMappings"] = block_device_mappings
 
         response = {
             'requestId': event['requestId'],
