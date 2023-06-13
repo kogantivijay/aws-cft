868ad981feacf5cee31b689a6fa1cb926a0f761f
Vijay Koganti <15212181+kogantivijay@users.noreply.github.com>
2023-06-13 09:11:40 +0100

    Add support for Tags

--- sg-transform/sg-helper-transform-macro.py
+++ sg-transform/sg-helper-transform-macro.py
@@ -8,15 +8,16 @@ logger.setLevel(logging.INFO)
 ssm = boto3.client('ssm')
 
 
-def transform_security_group_template(input_json):
+def transform_security_group_template(input_json, application):
     transformed_template = []
 
     for group in input_json:
         name = group['name']
         description = group.get('description', '')
         rules = group['rules']
-        transformed_rules = []
+        tags = group.get('tags', [])
 
+        transformed_rules = []
         for rule in rules:
             rule_type = rule['type']
             from_port = rule.get('fromPort')
@@ -26,54 +27,44 @@ def transform_security_group_template(input_json):
             source_security_group_id = resolve_ssm_parameter(rule.get('sourceSecurityGroupId', ''))
             destination_security_group_id = resolve_ssm_parameter(rule.get('destinationSecurityGroupId', ''))
 
+            # Validate fromPort and toPort
+            if rule_type == 'ingress' and (from_port is None or to_port is None):
+                raise ValueError("Missing 'fromPort' or 'toPort' in ingress rule.")
+            elif rule_type == 'egress' and (from_port is None or to_port is None):
+                raise ValueError("Missing 'fromPort' or 'toPort' in egress rule.")
+
+            if rule.get('ipProtocol') == '-1':
+                raise ValueError("Invalid value '-1' specified for 'ipProtocol' in rule.")
+
+            transformed_rule = {
+                'FromPort': from_port,
+                'ToPort': to_port,
+                'Description': description,
+                'IpProtocol': rule.get('ipProtocol', 'tcp')
+            }
+
             if rule_type == 'ingress':
-                if from_port is None or to_port is None:
-                    raise ValueError("Missing 'fromPort' or 'toPort' in ingress rule.")
-
-                ingress_rule = {}
-                ingress_rule['FromPort'] = from_port
-                ingress_rule['ToPort'] = to_port
-                ingress_rule['Description'] = description
-
-                if source_security_group_id:
-                    ingress_rule['SourceSecurityGroupId'] = source_security_group_id
-                elif cidr_ranges:
-                    ingress_rule['CidrIp'] = cidr_ranges[0]
-
-                if rule.get('ipProtocol') == '-1':
-                    raise ValueError("Invalid value '-1' specified for 'ipProtocol' in ingress rule.")
-
-                # Set default value for IpProtocol
-                ingress_rule['IpProtocol'] = rule.get('ipProtocol', 'tcp')
-
-                transformed_rules.append(ingress_rule)
-
-            elif rule_type == 'egress':
-                if from_port is None or to_port is None:
-                    raise ValueError("Missing 'fromPort' or 'toPort' in egress rule.")
-
-                egress_rule = {}
-                egress_rule['FromPort'] = from_port
-                egress_rule['ToPort'] = to_port
-                egress_rule['Description'] = description
-
                 if destination_security_group_id:
-                    egress_rule['DestinationSecurityGroupId'] = destination_security_group_id
+                    raise ValueError("Ingress rule should not have 'destinationSecurityGroupId'.")
+                if source_security_group_id:
+                    transformed_rule['SourceSecurityGroupId'] = source_security_group_id
                 elif cidr_ranges:
-                    egress_rule['CidrIp'] = cidr_ranges[0]
+                    transformed_rule['CidrIp'] = cidr_ranges[0]
+            elif rule_type == 'egress':
+                if source_security_group_id:
+                    raise ValueError("Egress rule should not have 'sourceSecurityGroupId'.")
+                if destination_security_group_id:
+                    transformed_rule['DestinationSecurityGroupId'] = destination_security_group_id
+                elif cidr_ranges:
+                    transformed_rule['CidrIp'] = cidr_ranges[0]
 
-                if rule.get('ipProtocol') == '-1':
-                    raise ValueError("Invalid value '-1' specified for 'ipProtocol' in egress rule.")
-
-                # Set default value for IpProtocol
-                egress_rule['IpProtocol'] = rule.get('ipProtocol', 'tcp')
-
-                transformed_rules.append(egress_rule)
+            transformed_rules.append(transformed_rule)
 
         transformed_group = {
             'name': name,
             'description': description,
-            'rules': transformed_rules
+            'rules': transformed_rules,
+            'tags': [{'Key': tag['Key'], 'Value': tag['Value']} for tag in tags] + [{'Key': 'iamfilter', 'Value': application}]
         }
 
         transformed_template.append(transformed_group)
@@ -82,13 +73,9 @@ def transform_security_group_template(input_json):
 
 
 def resolve_ssm_parameter(parameter):
-    # Placeholder logic to resolve SSM parameter
-    # Replace with your actual SSM parameter resolution logic
-    # For example, you can use Boto3 to fetch the parameter value
     if parameter.startswith('ssm:'):
         ssm_parameter_name = parameter[4:]
         try:
-            # Fetch the SSM parameter value using Boto3
             response = ssm.get_parameter(Name=ssm_parameter_name, WithDecryption=True)
             parameter_value = response['Parameter']['Value']
             return parameter_value
@@ -103,24 +90,37 @@ def lambda_handler(event, context):
     try:
         logger.info('Transform Security Group Template')
         input_json = event['input']
+        application = event['application']
         logger.info(f"Input JSON: {json.dumps(input_json, indent=2)}")
 
-        transformed_template = transform_security_group_template(input_json)
+        transformed_template = transform_security_group_template(input_json, application)
         logger.info(f"Transformed Template: {json.dumps(transformed_template, indent=2)}")
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps(transformed_template)
+        fragment = {
+            'requestId': event['requestId'],
+            'status': 'success',
+            'fragment': transformed_template
         }
+        return fragment
+
     except ValueError as e:
         logger.error(f"Error: {str(e)}")
-        return {
-            'statusCode': 400,
-            'body': str(e)
+        fragment = {
+            'requestId': event['requestId'],
+            'status': 'failure',
+            'fragment': {},
+            'errorMessage': str(e),
+            'errorType': type(e).__name__
         }
+        return fragment
+
     except Exception as e:
         logger.error(f"Unexpected error: {str(e)}")
-        return {
-            'statusCode': 500,
-            'body': "An unexpected error occurred."
+        fragment = {
+            'requestId': event['requestId'],
+            'status': 'failure',
+            'fragment': {},
+            'errorMessage': "An unexpected error occurred.",
+            'errorType': type(e).__name__
         }
+        return fragment
